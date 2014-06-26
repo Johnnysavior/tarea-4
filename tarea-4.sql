38fe8f894298a0804a6ed03ad90c3297aa39a7d6
pancha <Fsapiains@yahoo.com>
2014-06-26 14:42:05 -0400

    Subida sql tarea
    script tablas

---
+++ tarea-4.sql
@@ -0,0 +1,19 @@
+﻿create table bodega(
+
+  id serial not null,
+  nombre_bodega character(255),
+  codigo_bodega character(255),
+  unique (nombre_bodega , codigo_bodega),
+  primary key (id)
+);
+
+create table producto(
+
+   id serial not null,
+   nombre_producto character(255),
+   codigo_producto character(255),
+   precio_neto character (255),
+   fk_bodega_id serial not null references bodega(id) on update cascade on delete cascade,
+   unique (nombre_producto , codigo_producto),
+   primary key (id)
+);
